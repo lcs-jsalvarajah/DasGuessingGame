b996681ffc5b9c9ef89a8449117073df16eb202d
Salvarajah, Jheeven <jsalvarajah@lcs-mba3020.lcs.on.ca>
2018-01-16 13:56:57 -0500

    Added   outputs

--- Das Guessing Game/ViewController.swift
+++ Das Guessing Game/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet weak var numberInputed: UITextField!
+    
+    //MARK: Properties (Variables)
+    
+    //MARK: Overides
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: Actions
+    @IBAction func guessSubmitted(_ sender: Any) {
+        //Print the number that was guessed
+        print(numberInputed.text)
+        //Use a guard statement to bind to a non-optional variable
+        guard let inputGiven = numberInputed.text else {
+            //If the input was nil, stop and return (exit) the function
+            return
+        }
+        //Use a guard statement to atteplt to create and integer
+        guard let integerGiven = Int(inputGiven) else {
+            // If the input was text (E.g: "Five") we can't make and integer, so stop and return (exit) the function
+            return
+        }
+        
+        //Print the number that was guessed again
+        print(integerGiven)
+        
+    }
+    //MARK: Custom Function(s)
 
 }
 
